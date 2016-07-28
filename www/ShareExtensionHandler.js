9ff5d40a3ab95aa15fa1387932bdc49803065ef3
Oleg Shvarz <svarts@ukr.net>
2016-07-28 19:47:14 +0300

    Update ShareExtensionHandler.js

--- www/ShareExtensionHandler.js
+++ www/ShareExtensionHandler.js
@@ -1,18 +1,16 @@
-cordova.define("cordova-share-extension-handler.ShareExtensionHandler", function(require, exports, module) {
-   module.exports = {
-	   getJsonDataFromSharedPkpassFile: function(params, successCallback) {
-		   cordova.exec(successCallback,
-		                null,
-		                "ShareExtensionHandler",
-		                "getJsonDataFromSharedPkpassFile",
-		                [params]);
-	   },
-	   deletePkpass: function(params, successCallback) {
-		   cordova.exec(successCallback,
-		                null,
-		                "ShareExtensionHandler",
-		                "deletePkpass",
-		                [params]);
-	   }
-   };
-});
+module.exports = {
+   getJsonDataFromSharedPkpassFile: function(params, successCallback) {
+	   cordova.exec(successCallback,
+	                null,
+	                "ShareExtensionHandler",
+	                "getJsonDataFromSharedPkpassFile",
+	                [params]);
+   },
+   deletePkpass: function(params, successCallback) {
+	   cordova.exec(successCallback,
+	                null,
+	                "ShareExtensionHandler",
+	                "deletePkpass",
+	                [params]);
+   }
+};
